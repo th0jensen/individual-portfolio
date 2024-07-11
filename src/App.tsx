9aba3dea8ee6967fa05c4cc5e96d4be384872081
Thomas Jensen <thomas.jensen_@outlook.com>
2024-07-11 14:33:05 +0200

    Added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import Biography from './pages/Biography'
 import Education from './pages/Education'
 import Projects from './pages/Projects'
 import Stack from './pages/Stack'
-import portfolioData, { Data } from './data'
+// import portfolioData, { Data } from './data'
 
 function App() {
-    const [data, setData] = useState<Data>(portfolioData)
+    // const [data, setData] = useState<Data>(portfolioData)
 
     return (
         <>
